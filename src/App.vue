cf297f634e50196b93a86c325cc4666e97c34dcf
Samuel Maculuve <32066945+SamuelMaculuve@users.noreply.github.com>
2024-04-19 15:56:49 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header class="header navbar navbar-expand-lg bg-light shadow-sm fixed-top">
+    <div class="container px-3">
+      <a href="#" class="navbar-brand pe-3">
+        <img src="@/assets/logo.svg"  width="47" alt="Silicon">
+        Silicon
+      </a>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
